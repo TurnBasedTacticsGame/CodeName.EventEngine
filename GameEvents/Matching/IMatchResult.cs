aa5e825d6ad365dae92b6731a3e533647fe67f86
Exanite <exanite78012@gmail.com>
2023-08-07 18:40:37 -0400

    Remove IMatchResult (was never used by itself)

--- GameEvents/Matching/IMatchResult.cs
+++
@@ -1,7 +0,0 @@
-namespace CodeName.EventSystem.GameEvents.Matching
-{
-    public interface IMatchResult
-    {
-        public bool IsSuccess { get; }
-    }
-}
--- GameEvents/Matching/INodeMatchResult.cs
+++ GameEvents/Matching/INodeMatchResult.cs
@@ -1,7 +1,9 @@
 namespace CodeName.EventSystem.GameEvents.Matching
 {
-    public interface INodeMatchResult<TGameState> : IMatchResult
+    public interface INodeMatchResult<TGameState>
     {
+        public bool IsSuccess { get; }
+
         public GameStateTracker<TGameState> Tracker { get; }
         public GameEventNode<TGameState> Node { get; }
     }
