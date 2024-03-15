50584fec599a1aca7fcdee2299da9e593ecf01cb
Exanite <exanite78012@gmail.com>
2024-03-15 03:50:45 -0400

    Use NullOpSerializer to prevent cloning

--- GameEvents/ConstGameStateTracker.cs
+++ GameEvents/ConstGameStateTracker.cs
@@ -1,10 +1,11 @@
+using System;
 using CodeName.EventSystem.Tasks;
 
 namespace CodeName.EventSystem.GameEvents
 {
     public class ConstGameStateTracker<TGameState> : GameStateTracker<TGameState>
     {
-        public ConstGameStateTracker(TGameState state, GameStateTrackerConfig<TGameState> config) : base(state, config, false)
+        public ConstGameStateTracker(TGameState state, GameStateTrackerConfig<TGameState> config) : base(state, UseNullOpSerializer(config))
         {
             Config = config;
             Events = new GameEventTracker<TGameState>(config.Serializer);
@@ -24,6 +25,32 @@ namespace CodeName.EventSystem.GameEvents
                 await OnEventApplied(currentNode);
             }
             Events.Pop();
+        }
+
+        private static GameStateTrackerConfig<TGameState> UseNullOpSerializer(GameStateTrackerConfig<TGameState> config)
+        {
+            var result = config.ShallowCopy();
+            result.Serializer = new NullOpSerializer();
+
+            return result;
+        }
+
+        private class NullOpSerializer : ISerializer
+        {
+            public string Serialize(object value)
+            {
+                throw new NotSupportedException();
+            }
+
+            public T Deserialize<T>(string data)
+            {
+                throw new NotSupportedException();
+            }
+
+            public T Clone<T>(T value)
+            {
+                return value;
+            }
         }
     }
 }
