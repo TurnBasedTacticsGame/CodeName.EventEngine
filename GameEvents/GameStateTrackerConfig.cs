a8b9a36e2383a286454deda9e1ae43c2d8e3b155
Exanite <exanite78012@gmail.com>
2024-03-15 02:39:57 -0400

    Add GameStateTracker.ShallowCopy()

--- GameEvents/GameStateTrackerConfig.cs
+++ GameEvents/GameStateTrackerConfig.cs
@@ -7,5 +7,15 @@ namespace CodeName.EventSystem.GameEvents
         public bool IsDebugMode { get; set; } = false;
         public ISerializer Serializer { get; set; }
         public List<IGameEventHandler<TGameState>> GameEventHandlers { get; set; }
+
+        public GameStateTrackerConfig<TGameState> ShallowCopy()
+        {
+            return new GameStateTrackerConfig<TGameState>
+            {
+                IsDebugMode = IsDebugMode,
+                Serializer = Serializer,
+                GameEventHandlers = GameEventHandlers,
+            };
+        }
     }
 }
