cf3fcabeeaa654196cf9fefbf27250c8c97bfea8
Exanite <exanite78012@gmail.com>
2024-04-27 19:35:05 -0400

    Move doc comment

--- IGameStateTracker.cs
+++ IGameStateTracker.cs
@@ -12,6 +12,16 @@ namespace CodeName.EventSystem
         public GameEventTracker<TGameState> Events { get; }
         public GameEventNode<TGameState> CurrentNode { get; }
 
+        /// <summary>
+        /// Raise an event to be applied.
+        /// <para/>
+        /// Note: An event can be Prevented. Raising an event does not guarantee it will be applied.
+        /// <para/>
+        /// When an event is raised, 3 events will be called: <br/>
+        /// 1. OnEventRaised - Use to prevent events from being applied. <br/>
+        /// 2. OnEventConfirmed - Use to react to events before they are applied. <br/>
+        /// 3. OnEventApplied - Use to react to events after they are applied.
+        /// </summary>
         public StateTask RaiseEvent(GameEvent<TGameState> gameEvent);
     }
 }
