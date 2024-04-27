520606446dcd037e0fd3dac9c77d7563c18cde6b
Exanite <exanite78012@gmail.com>
2024-04-27 19:34:55 -0400

    Reorder code

--- IGameStateTracker.cs
+++ IGameStateTracker.cs
@@ -6,9 +6,10 @@ namespace CodeName.EventSystem
     public interface IGameStateTracker<TGameState>
     {
         public GameStateTrackerConfig<TGameState> Config { get; }
-        public GameEventTracker<TGameState> Events { get; }
 
         public TGameState State { get; }
+        
+        public GameEventTracker<TGameState> Events { get; }
         public GameEventNode<TGameState> CurrentNode { get; }
 
         public StateTask RaiseEvent(GameEvent<TGameState> gameEvent);
