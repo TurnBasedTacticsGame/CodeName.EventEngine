36cef32abc461540506b3dfb9286cd39b3b8b1ca
Exanite <exanite78012@gmail.com>
2024-06-04 07:06:01 -0400

    Merge branch 'dev/entities'

--- GameEvents/Matching/INodeMatchResult.cs
+++ GameEvents/Matching/INodeMatchResult.cs
@@ -1,5 +1,9 @@
 namespace CodeName.EventEngine.GameEvents.Matching
 {
+    /// <remarks>
+    /// Since this is an interface and most MatchResults are implemented as structs, the use of this interface will cause boxing in certain cases.
+    /// However, implementing MatchResults as classes will cause more allocations and pooling is difficult.
+    /// </remarks>
     public interface INodeMatchResult<TGameState>
     {
         public bool IsSuccess { get; }
